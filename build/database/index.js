e1c7e0041f732a8946453ca3a6da03a8e33a1282
BirdTool <93103818+BirdTool@users.noreply.github.com>
2025-03-15 09:04:37 -0300

    Delete build directory

--- build/database/index.js
+++
@@ -1,7 +0,0 @@
-import { QuickDB } from "quick.db";
-const filePath = rootTo("localdb.sqlite");
-const db = {
-    guilds: new QuickDB({ filePath, table: "guilds" }),
-    members: new QuickDB({ filePath, table: "members" })
-};
-export { db };
